858c97307bd2375d9cd8383e2f62bbccd0d1e9ab
Mac <mac@MacBook-Air-Mac.local>
2022-11-28 16:56:02 +0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void gitmain(String[] args) {
 
 
         try {
